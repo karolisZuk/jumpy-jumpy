0a531f5e3ac5339c590c008feca7da3c89be7536
karolisZuk <kar.zukas@gmail.com>
2022-02-05 09:29:36 +0100

    initial commit

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementController : MonoBehaviour
+{
+    PlayerInputActions playerInputActions;
+    CharacterController characterController;
+    Animator animator;
+
+    Vector2 currentMovementInput;
+    Vector3 currentMovement;
+    Vector3 currentRunMovement;
+
+    bool isMovementPressed;
+    bool isRunPressed;
+    float rotationFactorPerFrame = 15;
+    float runMultiplier = 3f;
+
+    // hashed animator parameters
+    int isWalkingHash;
+    int isRunningHash;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+
+        playerInputActions.CharacterControls.Move.started += OnMovementInput;
+        playerInputActions.CharacterControls.Move.canceled += OnMovementInput;
+        playerInputActions.CharacterControls.Move.performed += OnMovementInput;
+
+        playerInputActions.CharacterControls.Run.started += OnRun;
+        playerInputActions.CharacterControls.Run.canceled += OnRun;
+    }
+
+    void HandleGravity()
+    {
+        if(characterController.isGrounded)
+        {
+            float groundedGravity = -0.5f;
+            currentMovement.y = groundedGravity;
+            currentRunMovement.y = groundedGravity;
+        } else
+        {
+            float gravity = -9f;
+            currentMovement.y = gravity;
+            currentRunMovement.y = gravity;
+        }
+    }
+
+    void HandleAnimation()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+
+        if(isMovementPressed && !isWalking)
+            animator.SetBool(isWalkingHash, true);
+        else if (!isMovementPressed && isWalking)
+            animator.SetBool(isWalkingHash, false);
+
+        if((isMovementPressed && isRunPressed) && !isRunning)
+            animator.SetBool(isRunningHash, true);
+        else if ((!isMovementPressed || !isRunPressed) && isRunning)
+            animator.SetBool(isRunningHash, false);
+    }
+
+    void HandleRotation()
+    {
+        Vector3 positionToLookAt;
+
+        positionToLookAt.x = currentMovement.x;
+        positionToLookAt.y = 0.0f;
+        positionToLookAt.z = currentMovement.z;
+
+        Quaternion currentRotation = transform.rotation;
+
+        if(isMovementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+        }
+
+    }
+
+    void OnRun(InputAction.CallbackContext ctx)
+    {
+        isRunPressed = ctx.ReadValueAsButton();
+    }
+
+    void OnMovementInput(InputAction.CallbackContext ctx)
+    {
+        currentMovementInput = ctx.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.z = currentMovementInput.y;
+
+        currentRunMovement.x = currentMovementInput.x * runMultiplier;
+        currentRunMovement.z = currentMovementInput.y * runMultiplier;
+
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+    }
+
+    private void Update()
+    {
+        HandleRotation();
+        HandleAnimation();
+        HandleGravity();
+
+        if (isRunPressed)
+        {
+            characterController.Move(currentRunMovement * Time.deltaTime);
+        } else
+        {
+            characterController.Move(currentMovement * Time.deltaTime);
+        }
+    }
+
+    private void OnEnable()
+    {
+        playerInputActions.CharacterControls.Enable();
+    }
+
+
+    private void OnDisable()
+    {
+        playerInputActions.CharacterControls.Disable();
+    }
+}
