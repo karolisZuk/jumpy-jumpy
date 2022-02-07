cf6d856e03eba373626b146b7a19a099387bf4a0
karolisZuk <kar.zukas@gmail.com>
2022-02-07 20:12:52 +0100

    feat: split input controller and movement scripts

---
+++ Assets/Scripts/Locomotion.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locomotion : MonoBehaviour
+{
+    PlayerInputController input;
+    CharacterController characterController;
+    Animator animator;
+
+    Vector3 currentMovement;
+    Vector3 currentRunMovement;
+    Vector3 appliedMovement;
+
+    int isRunningHash;
+    int isWalkingHash;
+
+    [Header("Rotation")]
+    [Range(1, 100)] public float rotationSpeed = 25f;
+    [Header("Movement")]
+    [Range(1, 20)] public float walkMultiplier = 1.2f;
+    [Range(1, 20)] public float runMultiplier = 3f;
+
+    private void Awake()
+    {
+        input = GetComponent<PlayerInputController>();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+        HandleAnimation();
+    }
+
+    void HandleAnimation()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+
+        if (input.IsMovementPressed() && !isWalking)
+            animator.SetBool(isWalkingHash, true);
+        else if (!input.IsMovementPressed() && isWalking)
+            animator.SetBool(isWalkingHash, false);
+
+        if ((input.IsMovementPressed() && input.IsRunPressed()) && !isRunning)
+            animator.SetBool(isRunningHash, true);
+        else if ((!input.IsMovementPressed() || !input.IsRunPressed()) && isRunning)
+            animator.SetBool(isRunningHash, false);
+    }
+
+    void HandleMovement()
+    {
+        currentMovement.x = input.CurrentMovementInput().x * walkMultiplier;
+        currentMovement.z = input.CurrentMovementInput().y * walkMultiplier;
+
+        currentRunMovement.x = input.CurrentMovementInput().x * runMultiplier;
+        currentRunMovement.z = input.CurrentMovementInput().y * runMultiplier;
+
+        if (input.IsRunPressed())
+        {
+            input.appliedMovement.x = currentRunMovement.x;
+            input.appliedMovement.z = currentRunMovement.z;
+        }
+        else
+        {
+            input.appliedMovement.x = currentMovement.x;
+            input.appliedMovement.z = currentMovement.z;
+        }
+    }
+
+    void HandleRotation()
+    {
+        if(input.IsMovementPressed())
+        {
+            Vector3 positionToLookAt;
+
+            positionToLookAt.x = currentMovement.x;
+            positionToLookAt.y = 0.0f;
+            positionToLookAt.z = currentMovement.z;
+
+            Quaternion currentRotation = transform.rotation;
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+
+            transform.rotation = Quaternion.Slerp(
+                currentRotation,
+                targetRotation,
+                rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    PlayerInputActions playerInputActions;
+    CharacterController characterController;
+
+    Vector2 currentMovementInput;
+    bool isMovementPressed;
+    bool isRunPressed;
+    bool isJumpPressed;
+
+    [SerializeField] private bool alwaysRun;
+
+    [HideInInspector] public Vector3 appliedMovement;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        characterController = GetComponent<CharacterController>();
+
+        playerInputActions.CharacterControls.Move.started += OnMovementInput;
+        playerInputActions.CharacterControls.Move.canceled += OnMovementInput;
+        playerInputActions.CharacterControls.Move.performed += OnMovementInput;
+
+        playerInputActions.CharacterControls.Run.started += OnRun;
+        playerInputActions.CharacterControls.Run.canceled += OnRun;
+
+        playerInputActions.CharacterControls.Jump.started += OnJump;
+        playerInputActions.CharacterControls.Jump.canceled += OnJump;
+    }
+
+    private void Update()
+    {
+        characterController.Move(appliedMovement * Time.deltaTime);
+    }
+
+    void OnMovementInput(InputAction.CallbackContext ctx)
+    {
+        currentMovementInput = ctx.ReadValue<Vector2>();
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+    }
+
+    void OnRun(InputAction.CallbackContext ctx)
+    {
+        isRunPressed = ctx.ReadValueAsButton();
+    }
+
+    void OnJump(InputAction.CallbackContext ctx)
+    {
+        isJumpPressed = ctx.ReadValueAsButton();
+    }
+
+    public Vector2 CurrentMovementInput()
+    {
+        return currentMovementInput;
+    }
+
+    public bool IsMovementPressed()
+    {
+        return isMovementPressed;
+    }
+
+    public bool IsRunPressed()
+    {
+        return isRunPressed || alwaysRun;
+    }
+
+    public bool IsJumpPressed()
+    {
+        return isJumpPressed;
+    }
+
+    private void OnEnable()
+    {
+        playerInputActions.CharacterControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputActions.CharacterControls.Disable();
+    }
+}
