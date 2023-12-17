cccc9df32165757f22dced1e2e994a911245101b
JohnDoe <email@mail.com>
2023-12-17 15:47:35 +0100

    feat: updated movement system

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class MovementController : MonoBehaviour
-{
+public class MovementController : MonoBehaviour {
     PlayerInputActions playerInputActions;
     CharacterController characterController;
-    Animator animator;
+    [SerializeField] private Animator animator;
 
     Vector2 currentMovementInput;
     Vector3 currentMovement;
@@ -17,10 +16,16 @@ public class MovementController : MonoBehaviour
     #region Walking and Running Variables
     bool isMovementPressed;
     bool isRunPressed;
+
     [Header("Movement")]
     [Range(1, 20)] public float walkMultiplier = 1.2f;
     [Range(1, 40)] public float rotationFactorPerFrame = 15;
     [Range(1, 20)] public float runMultiplier = 3f;
+    [Range(0, 1)] public float smoothInputSpeed;
+    [SerializeField] private bool alwaysRun = true;
+
+    private Vector2 currentVectorInput;
+    private Vector2 smoothInputVelocity;
     #endregion
 
     #region Animation Hashes
@@ -31,6 +36,7 @@ public class MovementController : MonoBehaviour
     #region Gravity
     float gravity = -9f;
     float groundedGravity = -0.5f;
+
     [Header("Gravity")]
     [Range(1f, 10)] public float fallMultiplier = 2f;
     #endregion
@@ -38,23 +44,31 @@ public class MovementController : MonoBehaviour
     #region Jumping Variables
     bool isJumpPressed = false;
     float initialJumpVelocity;
+
     [Header("Jumping")]
     [Range(0.1f, 20)] public float maxJumpHeight = 0.5f;
     [Range(0.1f, 20)] public float maxJumpTime = 1.0f;
+    [SerializeField] private LayerMask environment;
     bool isJumping = false;
     int isJumpingHash;
+    int isLandingHash;
     bool isJumpAnimating = false;
+    bool isLandingAnimating = false;
     #endregion
 
-    private void Awake()
-    {
+    #region Dodge
+    bool isDodgePressed = false;
+    bool isDodging = false;
+    #endregion
+
+    private void Awake() {
         playerInputActions = new PlayerInputActions();
         characterController = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
 
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
         isJumpingHash = Animator.StringToHash("isJumping");
+        isLandingHash = Animator.StringToHash("isLanding");
 
         playerInputActions.CharacterControls.Move.started += OnMovementInput;
         playerInputActions.CharacterControls.Move.canceled += OnMovementInput;
@@ -66,66 +80,120 @@ public class MovementController : MonoBehaviour
         playerInputActions.CharacterControls.Jump.started += OnJump;
         playerInputActions.CharacterControls.Jump.canceled += OnJump;
 
+        playerInputActions.CharacterControls.Dodge.started += OnDodge;
+        playerInputActions.CharacterControls.Dodge.canceled += OnDodge;
+
         SetupJumpVariables();
+
+        if (alwaysRun) {
+            isRunPressed = true;
+        }
     }
 
-    void SetupJumpVariables()
-    {
+    private void Update() {
+        HandleRotation();
+        HandleAnimation();
+
+        currentVectorInput = Vector2.SmoothDamp(currentVectorInput, currentMovementInput, ref smoothInputVelocity, smoothInputSpeed);
+
+        currentMovement.x = currentVectorInput.x * walkMultiplier;
+        currentMovement.z = currentVectorInput.y * walkMultiplier;
+
+        currentRunMovement.x = currentVectorInput.x * runMultiplier;
+        currentRunMovement.z = currentVectorInput.y * runMultiplier;
+
+        if(isLandingAnimating) {
+            currentMovement.x = 0;
+            currentMovement.z = 0;
+            currentRunMovement.x = 0;
+            currentRunMovement.z = 0;
+        }
+
+        if (isRunPressed) {
+            appliedMovement.x = currentRunMovement.x;
+            appliedMovement.z = currentRunMovement.z;
+        } else {
+            appliedMovement.x = currentMovement.x;
+            appliedMovement.z = currentMovement.z;
+        }
+
+        characterController.Move(appliedMovement * Time.deltaTime);
+        animator.SetFloat("InputMagnitude", currentVectorInput.magnitude);
+
+        HandleGravity();
+        HandleJump();
+    }
+
+    void SetupJumpVariables() {
         float timeToApex = maxJumpTime / 2;
         gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToApex, 2);
         initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
     }
 
-    void OnJump(InputAction.CallbackContext ctx)
-    {
+    void OnDodge(InputAction.CallbackContext ctx) {
+        isDodgePressed = ctx.ReadValueAsButton();
+    }
+
+    void OnJump(InputAction.CallbackContext ctx) {
         isJumpPressed = ctx.ReadValueAsButton();
     }
 
-    void HandleJump()
-    {
-        if(!isJumping && characterController.isGrounded && isJumpPressed)
-        {
+    void HandleJump() {
+        if (!isJumping && characterController.isGrounded && isJumpPressed) {
             isJumping = true;
             isJumpAnimating = true;
             animator.SetBool(isJumpingHash, true);
 
             currentMovement.y = initialJumpVelocity;
             appliedMovement.y = initialJumpVelocity;
-        } else if(!isJumpPressed && isJumping && characterController.isGrounded)
-        {
+
+            RaycastHit hit;
+            if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + characterController.height, transform.position.z), transform.TransformDirection(Vector3.up), out hit, 1f, environment)) {
+                isJumpPressed = false;
+            }
+
+        } else if (!isJumpPressed && isJumping && characterController.isGrounded) {
             isJumping = false;
         }
     }
 
-    void HandleGravity()
-    {
+    void HandleGravity() {
         bool isFalling = currentMovement.y <= 0f || !isJumpPressed;
 
-        if(characterController.isGrounded)
-        {
-            if(isJumpAnimating)
-            {
+        if (characterController.isGrounded) {
+            if (isJumpAnimating) {
                 animator.SetBool(isJumpingHash, false);
                 isJumpAnimating = false;
             }
 
+            if (isLandingAnimating) {
+                animator.SetBool(isLandingHash, false);
+                isLandingAnimating = false;
+            }
+
             currentMovement.y = groundedGravity;
             appliedMovement.y = groundedGravity;
-        } else if (isFalling)
-        {
+        } else if (isFalling) {
             float previousYVelocity = currentMovement.y;
             currentMovement.y = currentMovement.y + (gravity * fallMultiplier * Time.deltaTime);
             appliedMovement.y = Mathf.Max((previousYVelocity + currentMovement.y) * .5f, -20f);
-        } else
-        {
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 1f, environment)) {
+                if (!isLandingAnimating) {
+                    animator.SetBool(isLandingHash, true);
+                    isLandingAnimating = true;
+                }
+            }
+
+        } else {
             float previousYVelocity = currentMovement.y;
             currentMovement.y = currentMovement.y + (gravity * Time.deltaTime);
             appliedMovement.y = (previousYVelocity + currentMovement.y) * .5f;
         }
     }
 
-    void HandleAnimation()
-    {
+    void HandleAnimation() {
         bool isWalking = animator.GetBool(isWalkingHash);
         bool isRunning = animator.GetBool(isRunningHash);
 
@@ -140,8 +208,7 @@ public class MovementController : MonoBehaviour
             animator.SetBool(isRunningHash, false);
     }
 
-    void HandleRotation()
-    {
+    void HandleRotation() {
         Vector3 positionToLookAt;
 
         positionToLookAt.x = currentMovement.x;
@@ -150,59 +217,31 @@ public class MovementController : MonoBehaviour
 
         Quaternion currentRotation = transform.rotation;
 
-        if(isMovementPressed)
-        {
+        if(isMovementPressed) {
             Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
             transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
         }
 
     }
 
-    void OnRun(InputAction.CallbackContext ctx)
-    {
-        isRunPressed = ctx.ReadValueAsButton();
+    void OnRun(InputAction.CallbackContext ctx) {
+        if(alwaysRun) {
+            isRunPressed = true;
+        } else {
+            isRunPressed = ctx.ReadValueAsButton();
+        }
     }
 
-    void OnMovementInput(InputAction.CallbackContext ctx)
-    {
+    void OnMovementInput(InputAction.CallbackContext ctx) {
         currentMovementInput = ctx.ReadValue<Vector2>();
-        currentMovement.x = currentMovementInput.x * walkMultiplier;
-        currentMovement.z = currentMovementInput.y * walkMultiplier;
-
-        currentRunMovement.x = currentMovementInput.x * runMultiplier;
-        currentRunMovement.z = currentMovementInput.y * runMultiplier;
-
         isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
     }
 
-    private void Update()
-    {
-        HandleRotation();
-        HandleAnimation();
-
-        if (isRunPressed)
-        {
-            appliedMovement.x = currentRunMovement.x;
-            appliedMovement.z = currentRunMovement.z;
-        } else
-        {
-            appliedMovement.x = currentMovement.x;
-            appliedMovement.z = currentMovement.z;
-        }
-
-        characterController.Move(appliedMovement * Time.deltaTime);
-
-        HandleGravity();
-        HandleJump();
-    }
-
-    private void OnEnable()
-    {
+    private void OnEnable() {
         playerInputActions.CharacterControls.Enable();
     }
 
-    private void OnDisable()
-    {
+    private void OnDisable() {
         playerInputActions.CharacterControls.Disable();
     }
 }
---
+++ Assets/Scripts/Resurrection Plane.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResurrectionPlane : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other != null && other.CompareTag("Player")) {
+            Transform t = other.GetComponent<Transform>();
+            CharacterController cc = other.GetComponent<CharacterController>();
+
+            cc.enabled = false;
+            t.position = new Vector3(0, 1f, 0);
+            cc.enabled = true;
+        }
+    }
+}
