3641c4ecd865b632d5ff8c8ed8cfd1a85eadbe45
JohnDoe <email@mail.com>
2023-12-28 23:19:41 +0100

    feat: added virtual cursor for joystick

--- Assets/Scripts/Player/PlayerInputs.cs
+++ Assets/Scripts/Player/PlayerInputs.cs
@@ -1,20 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputs : MonoBehaviour {
-    private PlayerInputActions playerInputActions;
+    public enum GameDevice { KeyboardMouse, Gamepad }
     public static PlayerInputs Instance { get; private set; }
 
+    // TODO: Move all input action subscriptions to this file and fire custom events for each action
+    public event EventHandler OnGameDeviceChanged;
+
+    private PlayerInputActions playerInputActions;
+    private GameDevice activeGameDevice;
 
     private void Awake() {
         if (Instance != null && Instance != this) {
             Destroy(this);
-        } else {
-            Instance = this;
+            return;
         }
 
+        Instance = this;
+
+        Instance = this;
+        transform.parent = null;
+        DontDestroyOnLoad(gameObject);
+
         playerInputActions = new PlayerInputActions();
+        InputSystem.onActionChange += InputSystem_onActionChange;
+
+        playerInputActions.Enable();
+    }
+
+    private void InputSystem_onActionChange(object arg1, InputActionChange inputActionChange) {
+        if (inputActionChange == InputActionChange.ActionPerformed && arg1 is InputAction) {
+            InputAction inputAction = arg1 as InputAction;
+
+            if (inputAction.activeControl.device.displayName == "VirtualMouse") {
+                // Ignore virtual mouse
+                return;
+            }
+
+            if (inputAction.activeControl.device is Gamepad) {
+                if (activeGameDevice != GameDevice.Gamepad) {
+                    ChangeActiveGameDevice(GameDevice.Gamepad);
+                }
+            } else {
+                if (activeGameDevice != GameDevice.KeyboardMouse) {
+                    ChangeActiveGameDevice(GameDevice.KeyboardMouse);
+                }
+            }
+        }
+    }
+
+    private void ChangeActiveGameDevice(GameDevice gameDevice) {
+        activeGameDevice = gameDevice;
+        Debug.Log("New Active Game Device: " + activeGameDevice);
+
+        Cursor.visible = activeGameDevice == GameDevice.KeyboardMouse;
+
+        OnGameDeviceChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public GameDevice GetActiveGameDevice() {
+        return activeGameDevice;
     }
 
     public PlayerInputActions PlayerInputActions () {
@@ -23,5 +70,9 @@ public class PlayerInputs : MonoBehaviour {
         }
 
         return playerInputActions;
+    }
+
+    private void OnDestroy() {
+        InputSystem.onActionChange -= InputSystem_onActionChange;
     }
 }
---
+++ Assets/Scripts/UI/VirtualMouseUI.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.UI;
+
+public class VirtualMouseUI : MonoBehaviour {
+    public static VirtualMouseUI Instance { get; private set; }
+
+    [SerializeField] private RectTransform virtualMouse;
+    [SerializeField] private RectTransform canvasRectTransform;
+    [SerializeField] private float mouseBorderSize = 5f;
+    [SerializeField] private bool shouldCursorAlwaysBeUnlocked;
+    [SerializeField] private bool forceDisabled = false;
+
+    private VirtualMouseInput virtualMouseInput;
+
+    private void Awake() {
+        Instance = this;
+        virtualMouseInput = GetComponent<VirtualMouseInput>();
+
+    }
+
+    private void Start() {
+        PlayerInputs.Instance.OnGameDeviceChanged += Instance_OnGameDeviceChanged;
+
+        ResetMouseToCenter();
+        UpdateVisibility();
+    }
+
+    private void Update() {
+        transform.localScale = Vector3.one * (1f / canvasRectTransform.localScale.x);
+        transform.SetAsLastSibling();
+    }
+
+    private void LateUpdate() {
+        Vector2 virtualMousePosition = virtualMouseInput.virtualMouse.position.value;
+
+        virtualMousePosition.x = Mathf.Clamp(virtualMousePosition.x, 0f + mouseBorderSize, Screen.width - mouseBorderSize);
+        virtualMousePosition.y = Mathf.Clamp(virtualMousePosition.y, 0f + mouseBorderSize, Screen.height - mouseBorderSize);
+
+        InputState.Change(virtualMouseInput.virtualMouse.position, virtualMousePosition);
+    }
+
+    private void Instance_OnGameDeviceChanged(object sender, System.EventArgs e) {
+        UpdateVisibility();
+    }
+
+    private void UpdateVisibility() {
+        if((PlayerInputs.Instance.GetActiveGameDevice() == PlayerInputs.GameDevice.Gamepad || shouldCursorAlwaysBeUnlocked) && !forceDisabled) {
+            ResetMouseToCenter();
+            Show();
+        } else {
+            Hide();
+        }
+    }
+
+    private void ResetMouseToCenter() {
+        virtualMouse.anchoredPosition = new Vector2(Screen.width / 2f, Screen.height / 2f);
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide() {
+        try {
+            gameObject.SetActive(false);
+        } catch (Exception e) {
+            Debug.LogError("VirtualMouseUI: " + e);
+        }
+    }
+
+    private void OnDestroy() {
+        PlayerInputs.Instance.OnGameDeviceChanged -= Instance_OnGameDeviceChanged;
+
+    }
+}
